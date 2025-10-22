c582c28f22063a4ab09787ea891b59860ca3ddd9
Muhammad Ahmad <highonworking@gmail.com>
2025-10-22 18:56:26 +0500

    some work some not work :(

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Game_Screen import GameScreen
 from GameScore import my_score
 from MyWalkingTurtle import walking_player
 from enemy import enemy_cars
-
+import time
 MOVE_SPEED = 20
 
 num_of_turtles = None
@@ -23,26 +23,24 @@ def make_turtles():
     for i in range(0, num_of_turtles):
         new_car = enemy_cars()
         new_car.location_of_cars()
-        # new_car.x_y_values_of_cars()
         enemy.append(new_car)
         enemy[i].color(enemy[i].color_of_car())
 
 make_turtles() 
+
 while True:
     turtle_crossing_screen.My_Game_Screen.update()
     for i in range(0, num_of_turtles):
         enemy[i].move_car()
-        first_x_value = enemy[i].xcor()
-        second_x_value = first_x_value - 20
-        first_y_value = enemy[i].ycor()
-        second_y_value = first_y_value - 10
-        if enemy[i].first_x_value >= player.xcor() >= enemy[i].second_x_value and enemy[i].first_y_value >= player.ycor() >= enemy[i].second_y_value:
+        if abs(player.xcor() - enemy[i].xcor()) < 20 and abs(player.ycor() - enemy[i].ycor()) < 20:
             print("HIT")
+    make_turtles()
+    time.sleep(0.01)
     scored_points.give_score()
     if player.ycor() >= 250:
         scored_points.score_increase()
         player.goto(0, -280)
-        make_turtles()
+        
     
 
 turtle.mainloop()
