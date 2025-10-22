e40878242c22416bce36c67e653ae21bd748ad4e
Muhammad Ahmad <highonworking@gmail.com>
2025-10-22 18:16:29 +0500

    no work :(

--- enemy.py
+++ enemy.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 import random
-
+import time
 
 class enemy_cars(Turtle):
     def __init__(self):
@@ -8,7 +8,10 @@ class enemy_cars(Turtle):
         self.shape("square")
         self.shapesize(1, 2)
         self.penup()
-
+        self.first_x_value = self.xcor()
+        self.second_x_value = self.first_x_value - 20
+        self.first_y_value = self.ycor()
+        self.second_y_value = self.first_y_value - 10
     def color_of_car(self):
         r = random.randint(0, 255)
         g = random.randint(0, 255)
@@ -22,4 +25,5 @@ class enemy_cars(Turtle):
         self.goto(280, Y_POSITION)
     def move_car(self):
         random_speed_for_car = random.randint(0, 10)
-        self.forward(random_speed_for_car)
+        self.forward(-random_speed_for_car)
+        time.sleep(0.01)
--- main.py
+++ main.py
@@ -19,21 +19,30 @@ turtle_crossing_screen.My_Game_Screen.onkeypress(key="w", fun=player.move_turtle
 def make_turtles():
     global enemy
     global num_of_turtles
-    num_of_turtles = random.randint(0, 10)
+    num_of_turtles = random.randint(1, 10)
     for i in range(0, num_of_turtles):
         new_car = enemy_cars()
         new_car.location_of_cars()
+        # new_car.x_y_values_of_cars()
         enemy.append(new_car)
         enemy[i].color(enemy[i].color_of_car())
 
 make_turtles() 
 while True:
-    # for i in range(0, num_of_turtles):
-    #     enemy[i].move_car()
     turtle_crossing_screen.My_Game_Screen.update()
+    for i in range(0, num_of_turtles):
+        enemy[i].move_car()
+        first_x_value = enemy[i].xcor()
+        second_x_value = first_x_value - 20
+        first_y_value = enemy[i].ycor()
+        second_y_value = first_y_value - 10
+        if enemy[i].first_x_value >= player.xcor() >= enemy[i].second_x_value and enemy[i].first_y_value >= player.ycor() >= enemy[i].second_y_value:
+            print("HIT")
     scored_points.give_score()
     if player.ycor() >= 250:
         scored_points.score_increase()
         player.goto(0, -280)
+        make_turtles()
+    
 
 turtle.mainloop()
